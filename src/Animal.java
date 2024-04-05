f77455d6f801f2c3615ab1e24edce3400b5654ae
HaDaHyeon <hada3817@gmail.com>
2024-04-06 02:18:33 +0900

    first

---
+++ src/Animal.java
@@ -0,0 +1,5 @@
+public interface Animal {
+    final int leg=4; //final생략 가능, 생략 시 자바에서 자동으로 public static final로 간주함
+    void brak();
+    void eat();
+}
---
+++ src/Cat.java
@@ -0,0 +1,11 @@
+public class Cat implements Animal{
+    @Override
+    public void brak() {
+        System.out.println("냐옹");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("짭짭");
+    }
+}
